4553a463d1ee52f03c8fe6c4adb08160cfff893b
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-09-05 10:57:21 -0300

    correcao das condicoes nas linhas 8 e 11

--- exercicios/exercicio21.py
+++ exercicios/exercicio21.py
@@ -5,10 +5,10 @@ tipo = input("Digite C para Celsius e F para Farenheit: ")
 
 resultado = 0.0
 
-if tipo == "C" or "c":
+if tipo == "C" or tipo == "c":
     resultado = temperatura * 1.8 + 32
     print(temperatura, "ºC em Farenheit eh", resultado, "ºF")
-elif tipo == "F" or "f":
+elif tipo == "F" or tipo == "f":
     resultado = (temperatura - 32) / 1.8
     print(temperatura, "ºF em Celsius eh", resultado, "ºC")
 else:
