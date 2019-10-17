9cad1140a279f6cca1abe0c2be125d8337e62f79
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-10-17 11:08:47 -0300

    laços com texto

--- exercicios/vp2/exercicio-for-funcao2.py
+++ exercicios/vp2/exercicio-for-funcao2.py
@@ -2,3 +2,12 @@
 # preço de 10 produtos e que exibe os preços dos
 # produtos reajustados em 15%
 # Teste o funcionamento da funcao no final do codigo
+def reajustar(lista) :
+    for item in lista :
+        reajuste = item + item * 0.15
+        print(reajuste)
+
+
+produtos = [100.0, 51.99, 45.90, 34.45, 4055.50, 230.0, 230.3, 232.0, 2324.33, 231.33]
+
+reajustar(produtos)
